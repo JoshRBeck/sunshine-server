8b9c21f3b3ffa9400205ef41a0b3907bea384ec1
beckjosh <Joshbeck17@gmail.com>
2023-05-10 11:15:22 +0200

    Token Based Authentication Commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require("./db");
 const express = require("express");
 
 const app = express();
-
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
@@ -18,7 +18,15 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+const profileRoutes = require("./routes/profile");
+app.use("/profile", isAuthenticated, profileRoutes);
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
 
 module.exports = app;
