6c2f438e2478c9a32e7247ac975a95619dd343bd
Lukas Jülich <lukas-juelich@web.de>
2023-05-11 11:36:36 +0200

    set up server routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,77 @@
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const { isAuthenticated } = require("../middleware/jwt.middleware")
+
+const router = require("express").Router();
+const saltRounds = 10
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    console.log(req.body);
+    const verify = await User.findOne({ email });
+    if (verify) {
+      res.status(405).json({ message: "The email is already in use!" });
+    } else {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      console.log("hello");
+      await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+      res.status(201).json({ message: "User created" });
+    }
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status.json({ message });
+    }
+  }
+});
+
+// POST /auth/login - Verifies email and password and returns a JWT
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.status(400).json({ message: "Provide email and password" });
+    return;
+  }
+
+  User.findOne({ email })
+    .then((foundUser) => {
+      if (!foundUser) {
+        res.status(400).json({ message: "User not found" });
+        return;
+      }
+
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+      if (passwordCorrect) {
+        const { _id, email, name } = foundUser;
+
+        const payload = { _id, email, name };
+
+        // Create and sign the token
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        });
+
+        res.status(200).json({ authToken: authToken });
+      } else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+    })
+    .catch((err) => res.status(500).json({ message: "Interal Server Error" }));
+});
+
+// GET /auth/verify - Used to verify JWT stored on the client
+router.get("/verify", isAuthenticated, (req, res) => {
+  console.log("req.payload", req.payload);
+  res.status(200).json(req.payload);
+});
+
+module.exports = router;
