fe7ceb2d70e832fbb16c844fc7b491d0b4be6608
Lukas Jülich <lukas-juelich@web.de>
2023-05-17 10:12:14 +0200

    routes done

--- routes/wine.routes.js
+++ routes/wine.routes.js
@@ -4,6 +4,15 @@ const mongoose = require("mongoose")
 
 const Wine = require("../models/Wine.model")
 
+// POST /api/wines - Creates a new wine
+router.post("/wines", (req, res ) => {
+  const { name, type, attributes, variety, region } = req.body
+
+  Wine.create({ name, type, attributes, variety, region })
+    .then(response => res.json(response))
+    .catch(err => res.json(err))
+})
+
 // GET /api/wines - Retrieves all of the wines
 router.get("/wines", (req, res) => {
     Wine.find()
@@ -24,5 +33,37 @@ router.get("/wines/:wineId", (req, res) => {
       .then(wine => res.status(200).json(wine))
       .catch(err => res.json(err))
   })
+
+  // PUT /api/wines/:wineId - Updates a specific wine by id
+router.put("/wines/:wineId", (req, res) => {
+  const { wineId } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(wineId)) {
+    res.status(400).json({ message: "Specified id is not valid" })
+    return
+  }
+
+  Wine.findByIdAndUpdate(wineId, req.body, { new: true })
+    .then(updatedWine => res.json(updatedWine))
+    .catch(err => res.json(err))
+})
+
+// DELETE /api/wines/:wineId - Delete a specific wine by id
+router.delete("/wines/:wineId", (req, res) => {
+  const { wineId } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(wineId)) {
+    res.status(400).json({ message: "Specified id is not valid" })
+    return
+  }
+
+  // We should normally also delete the tasks for the project
+
+  Wine.findByIdAndDelete(wineId)
+    .then(() => {
+      res.json({ message: `Project with ${wineId} removed successfully` })
+    })
+    .catch(err => res.json(err))
+})
 
   module.exports = router
