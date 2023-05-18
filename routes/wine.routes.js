3713b9c3c48334257d648dee0c47685454085231
beckjosh <Joshbeck17@gmail.com>
2023-05-18 08:02:35 +0200

    Removed console logs

--- routes/wine.routes.js
+++ routes/wine.routes.js
@@ -1,28 +1,27 @@
 const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
-
-const Project = require("../models/Wine.model")
+const Wine = require("../models/Wine.model")
 
 // GET /api/projects - Retrieves all of the wines
 router.get("/wines", (req, res) => {
-    Wine.find()
-      .then(allWines => res.json(allWines))
-      .catch(err => res.json(err))
-  })
+  Wine.find()
+    .then(allWines => res.json(allWines))
+    .catch(err => res.json(err))
+})
 
-  // GET /api/wines/:wineId - Retrieves a specific wine by id
+// GET /api/wines/:wineId - Retrieves a specific wine by id
 router.get("/wines/:wineId", (req, res) => {
-    const { wineId } = req.params
-  
-    if(!mongoose.Types.ObjectId.isValid(wineId)) {
-      res.status(400).json({ message: "Specified id is not valid" })
-      return
-    }
-  
-    Wine.findById(wineId)
-      .then(wine => res.status(200).json(wine))
-      .catch(err => res.json(err))
-  })
+  const { wineId } = req.params
 
-  module.exports = router
+  if (!mongoose.Types.ObjectId.isValid(wineId)) {
+    res.status(400).json({ message: "Specified id is not valid" })
+    return
+  }
+
+  Wine.findById(wineId)
+    .then(wine => res.status(200).json(wine))
+    .catch(err => res.json(err))
+})
+
+module.exports = router
